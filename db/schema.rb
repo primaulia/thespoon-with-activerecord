3c458290787d1ce10db137d91fb210732b50011f
Prima Aulia Gusta <primaulia@gmail.com>
2019-11-19 09:23:23 +0800

    update chef name to restos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_011214) do
+ActiveRecord::Schema.define(version: 2019_11_19_012033) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_011214) do
     t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "chef_name"
   end
 
 end
